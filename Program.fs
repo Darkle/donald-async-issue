dd56b08a5d1c45cda4a734889a989d6e6a0dbaec
Coop <coop.coding@gmail.com>
2022-11-24 14:50:42 +1100

    initial commit

---
+++ Program.fs
@@ -0,0 +1,42 @@
+﻿open Microsoft.Data.Sqlite
+open Donald
+
+[<EntryPoint>]
+let main args =
+    // fsharplint:disable-next-line RedundantNewKeyword
+    let conn = new SqliteConnection("Filename=" + "./foo.db" + ";foreign keys=true")
+
+    let sql = "INSERT INTO Test (foo) VALUES (@foo);"
+
+    let sqlParams = [ ("foo", SqlType.String "bar") ]
+
+    let sqlToExec = conn |> Db.newCommand sql |> Db.setParams sqlParams
+
+    task {
+        try
+            let! dbResult = Db.Async.exec sqlToExec
+            printfn "dbResult %A" dbResult
+            ()
+        with err ->
+            printfn "DB Error: %A" err
+    }
+    |> ignore
+
+    let sql1 = "INSERT INTO Test (foo) VALUES (@foo);"
+
+    let sqlParams1 = [ ("non-existant-column", SqlType.String "bar") ]
+
+    let sqlToExec1 = conn |> Db.newCommand sql1 |> Db.setParams sqlParams1
+
+
+    task {
+        try
+            let! dbResult = Db.Async.exec sqlToExec1
+            printfn "dbResult %A" dbResult
+            ()
+        with err ->
+            printfn "DB Error: %A" err
+    }
+    |> ignore
+
+    0
